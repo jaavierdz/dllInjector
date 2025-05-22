9c66263297931688a72f6c6ec5f413b782299377
Javierdz (Through Visual Studio) <javier@javierdz.me>
2025-05-22 13:19:32 +0200

    Added more verbose

--- sexoanal.cpp
+++ sexoanal.cpp
@@ -1,9 +1,5 @@
 #include <windows.h>
 #include <stdio.h>
-
-
-
-
 /* Esto son las variables para debug (verbose) */
 const char* ok = "[+]";
 const char* inf = "[*]";
@@ -20,7 +16,7 @@ LPVOID rBuffer = NULL;
 unsigned char dll[] = "C:/Users/Techie12/source/repos/sexoanal/x64/debug/DLL1.dll";
 
 int main(int argc, char* argv[]) {
-	/* Comprobamos si por casualidad el usuario no ha especificado ningun PID*/
+	/* Comprobamos si por casualidad el usuario no ha especificado ningun PID */
 	if (argc < 2 ) {
 		printf("%s No process ID specified for injection\n", err);
 		return EXIT_FAILURE;
@@ -58,11 +54,21 @@ int main(int argc, char* argv[]) {
 	CreateRemoteThread(hProcess, 0, 0, (LPTHREAD_START_ROUTINE)loadLibraryAddr, rBuffer, 0, 0);
 	if (CreateRemoteThread == NULL) {
 		printf("%s Could not inject DLL to the PID (%ld), error %ld", err, PID, GetLastError());
+		printf("%s Variable written value = %ld", inf, wrote);
+		printf("%s Variable rBuffer address = %p", inf, rBuffer);
+		printf("%s Variable lpBuffer address = %p", inf, lpBuffer);
+		printf("%s Variable PID = %ld", inf, PID);
+		printf("%s Variable hProcess = %p", inf, hProcess);
+		printf("%s Variable hThread = %p", inf, hThread);
+		printf("%s Variable loadLibraryAddr = %p", inf, loadLibraryAddr);
+		
+		
 		return EXIT_FAILURE;
 	}
 	else {
-		printf("%s DLL injected into the PID (%ld)\n", ok, PID);
+		printf("%s DLL (WROTEMEM) injected into the PID (%ld)\n", ok, PID);
+
+
 	}
 	return EXIT_SUCCESS;
-	
 }
